5b6c9b69d059e93a8d94cda705958bee77cc9f59
Dr. ERDI Gergo <gergo@erdi.hu>
2020-02-22 17:32:19 +0800

    Fix _verilator-exe build

--- verilator/verilate.sh
+++ verilator/verilate.sh
@@ -1,6 +1,6 @@
 set -xe
 
-verilator -cc -CFLAGS "-O3 -fPIC" --clk CLK_25MHZ -Mdir _verilator-exe _build/clash/verilog/Bounce/Bounce/Bounce.v --exe SimMain.cpp
+verilator -cc -CFLAGS "-O3 -fPIC" --clk CLK_25MHZ -Mdir _verilator-exe _build/clash/verilog/Bounce/Bounce/Bounce.v --exe DummyMain.cpp
 (cd _verilator-exe && make -f VBounce.mk)
 
 verilator -cc -CFLAGS "-O3 -fPIC" --clk CLK_25MHZ -Mdir _verilator-lib _build/clash/verilog/Bounce/Bounce/Bounce.v SimStub.cpp
